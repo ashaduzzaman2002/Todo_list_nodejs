079b77486b7ed6111bee51ff37e27c2a40c17c36
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-01-19 15:37:51 +0530

    clear

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const flash = require('connect-flash')
 
 const app = express();
 
@@ -14,7 +13,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"));
-app.use(flash())
 
 app.use('/', todoRouter)
 app.use('/user', userRouter)
